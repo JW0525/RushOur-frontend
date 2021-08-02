38130120d5b6c05533a911202107401741def789
mateo <materokatti@gmail.com>
2021-08-02 20:23:09 +0900

    ADD: nav draft complete

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 
 class Routes extends Component {
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+import './Nav.scss';
 
 export class Nav extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="Nav">
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <h1>LUSH</h1>
+        <ul>
+          <li>제품</li>
+          <li>러쉬 소개</li>
+          <li>매장 안내</li>
+          <li>스파</li>
+          <li>이벤트</li>
+        </ul>
+        <div className="navIcon">
+          <i class="fas fa-search"></i>
+          <i class="fas fa-shopping-bag"></i>
+          <i class="fas fa-user-circle"></i>
+        </div>
+      </div>
+    );
   }
 }
 
