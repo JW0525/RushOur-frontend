4dc006c15274cbe6b2f58f00ed3d13f97d2d609c
mateo <materokatti@gmail.com>
2021-08-02 17:46:30 +0900

    Project first commit

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer';
+import Main from './pages/Main/Main';
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/main" Component={Main} />
+        </Switch>
+        <Footer />
+      </Router>
+    );
+  }
+}
+
+export default Routes;
