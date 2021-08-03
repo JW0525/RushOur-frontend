761c8ac16a540ca433ad0e6955b51b17977d964b
mateo <materokatti@gmail.com>
2021-08-03 13:56:24 +0900

    ADD: public 폴더 내부 images 폴더 추가

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import ListMenu from './ListMenu/ListMenu';
 import './ProductList.scss';
 
 class ProductList extends Component {
   render() {
-    console.log('render ProductList');
     return (
-      <div className="listHeader">
-        {/* <div className="listHeaderImg">dfsafsad</div> */}
-        <img src="/images/fakeHeader.png" alt="headerImg" />
-        <div className="listMenu">
-          <h2>body</h2>
+      <>
+        <div className="listHeader">
+          {/* <div className="listHeaderImg">dfsafsad</div> */}
+          <img src="/images/fakeHeader.png" alt="headerImg" />
         </div>
-      </div>
+
+        <ListMenu />
+      </>
     );
   }
 }
